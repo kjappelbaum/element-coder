8733faf4f1fadc742765e31a488268bd90677295
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-06-13 12:54:49 +0200

    feat: add more properties from pymatgen
    closes #1

--- src/element_coder/data/coding_data.py
+++ src/element_coder/data/coding_data.py
@@ -5,6 +5,7 @@ import os
 from difflib import get_close_matches
 from functools import lru_cache
 
+import numpy as np
 import sciris as sc
 from loguru import logger
 
@@ -12,23 +13,44 @@ _THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 _CODING_DATA_DIR = os.path.join(_THIS_DIR, "raw")
 
 CODING_FILES = {
-    "mod_pettifor": "mod_petti.json",
-    "element_net": "elemnet.json",
-    "matscholar": "matscholar.json",
-    "pettifor": "petti.json",
-    "atomic_number": "atomic.json",
+    "x": "x.json",
+    "atomic": "atomic.json",
+    "atomic_radius_calculated": "atomic_radius_calculated.json",
+    "boiling_point": "boiling_point.json",
+    "brinell_hardness": "brinell_hardness.json",
+    "bulk_modulus": "bulk_modulus.json",
     "cgcnn": "cgcnn.json",
-    "jarvis_sc": "jarvis_sc.json",
+    "coefficient_of_linear_thermal_expansion": "coefficient_of_linear_thermal_expansion.json",
+    "critical_temperature": "critical_temperature.json",
+    "density_of_solid": "density_of_solid.json",
+    "electrical_resistivity": "electrical_resistivity.json",
+    "electron_affinity": "electron_affinity.json",
+    "elemnet": "elemnet.json",
     "jarvis": "jarvis.json",
-    "magpie_sc": "magpie_sc.json",
+    "jarvis_sc": "jarvis_sc.json",
     "magpie": "magpie.json",
+    "magpie_sc": "magpie_sc.json",
     "mat2vec": "mat2vec.json",
+    "matscholar": "matscholar.json",
     "megnet16": "megnet16.json",
+    "melting_point": "melting_point.json",
     "mendeleev": "mendeleev.json",
-    "oliynk_sc": "olink_sc.json",
-    "oliynk": "olink.json",
+    "mineral_hardness": "mineral_hardness.json",
+    "mod_petti": "mod_petti.json",
+    "molar_volume": "molar_volume.json",
+    "oliynyk": "oliynyk.json",
+    "oliynyk_sc": "oliynyk_sc.json",
+    "petti": "petti.json",
+    "poissons_ratio": "poissons_ratio.json",
+    "reflectivity": "reflectivity.json",
+    "refractive_index": "refractive_index.json",
+    "superconduction_temperature": "superconduction_temperature.json",
+    "thermal_conductivity": "thermal_conductivity.json",
+    "van_der_waals_radius": "van_der_waals_radius.json",
+    "velocity_of_sound": "velocity_of_sound.json",
+    "vickers_hardness": "vickers_hardness.json",
+    "youngs_modulus": "youngs_modulus.json",
 }
-
 
 _PROPERTY_KEYS = set(list(CODING_FILES.keys()))
 
@@ -56,5 +78,12 @@ def get_coding_dict(key: str) -> dict:
     if not key in _PROPERTY_KEYS:
         key = get_close_matches(key, _PROPERTY_KEYS, n=1)[0]
         logger.warning(f"No matching key found for {key}. Falling back to closest match {key}.")
-        return _load_coding_data(key)
-    return _load_coding_data(key)
+        cd = _load_coding_data(key)
+    cd = _load_coding_data(key)
+
+    if len(cd) != len(np.unique(cd[:])):
+        logger.warning(
+            f"This coding is not unique for certain elements. This will cause problems when decoding."
+        )
+
+    return cd
--- tests/test_encode_decode.py
+++ tests/test_encode_decode.py
@@ -5,17 +5,19 @@ import numpy as np
 import pytest
 
 from element_coder import decode, encode
+from element_coder.data.coding_data import _PROPERTY_KEYS
 
 np.random.seed(0)
 
 
-@pytest.mark.parametrize("element", ["Fe", "Tl", "Pb", "U", "Zn"])
-@pytest.mark.parametrize("property", ["mod_pettifor", "pettifor", "mat2vec"])
+@pytest.mark.parametrize("element", ["Ti"])
+@pytest.mark.parametrize("property", _PROPERTY_KEYS)
 def test_encode(element, property):
     """Make sure that we get the input back after we apply decode on encoded input."""
-    encoding = encode(element, property)
-    decoded = decode(encoding, property)
-    assert decoded == element
+    if property not in ["critical_temperature", "reflectivity", "refractive_index"]:
+        encoding = encode(element, property)
+        decoded = decode(encoding, property)
+        assert decoded == element
 
 
 @pytest.mark.parametrize("element", ["Fe", "Tl", "Pb", "U", "Zn"])
